56be604ceb60f9e26566680433c115faa02a965a
belemaaa <graceitamunoala@gmail.com>
2023-08-12 18:38:46 +0000

    echoed data

---
+++ backend/cfehome/api/views.py
@@ -0,0 +1,17 @@
+import json
+from django.http import JsonResponse
+
+
+def api_home(request, *args, **kwargs):
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    print(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
