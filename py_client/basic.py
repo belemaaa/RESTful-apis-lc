dfa028654db3e53f87cdba8676b7f076e3f33b5f
belemaaa <graceitamunoala@gmail.com>
2023-08-13 20:33:30 +0000

    model

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={
+get_response = requests.get(endpoint, params={"abc": 123}, json={
     "name": "grace",
 })
 print(get_response.json())
