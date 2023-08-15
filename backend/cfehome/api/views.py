d3221dca9bd753cfca2a7126360a1086a7cff015
belemaaa <graceitamunoala@gmail.com>
2023-08-15 18:29:45 +0000

    injesting data - serializing client data

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -7,11 +7,11 @@ from products.models import Product
 from products.serializers import ProductSerialzer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        #data = model_to_dict(instance)
-        data = ProductSerialzer(instance).data
-    return Response(data)
+    serializer = ProductSerialzer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        print(instance)
+
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={'product_id': 123})
+get_response = requests.post(endpoint, json={'title': 'Hello world', 'content': 'a good day'})
 print(get_response.json())
