f9602156d55cae9e693254b01a4d1b5a98634045
belemaaa <graceitamunoala@gmail.com>
2023-08-15 08:54:27 +0000

    serializers

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -2,16 +2,16 @@ import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from products.models import Product
+from products.serializers import ProductSerialzer
 
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data)
-
+    if instance:
+        #data = model_to_dict(instance)
+        data = ProductSerialzer(instance).data
     return Response(data)
