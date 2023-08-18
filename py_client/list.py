9ae6d293821e15949e110b7432aeab6acdec03d4
belemaaa <graceitamunoala@gmail.com>
2023-08-18 10:14:26 +0000

    correction to auth client data

--- py_client/list.py
+++ py_client/list.py
@@ -14,12 +14,8 @@ print(auth_response.json())
 if auth_response.status_code == 200:
     endpoint = "http://localhost:8000/api/products/"
     token = auth_response.json()['token']
-
-    data = {
-        "title": "this is an addition to the database"
-    }
     headers = {
         'Authorization': f'Token {token}'
     }
-    get_response = requests.get(endpoint, json=data, headers=headers)
+    get_response = requests.get(endpoint, headers=headers)
     print(get_response.json())
