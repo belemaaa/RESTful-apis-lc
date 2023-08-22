3a95d982f96b40f5e97e7b3b5e82ecf37611ffaa
belemaaa <graceitamunoala@gmail.com>
2023-08-22 17:36:42 +0000

    commit

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerialzer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
+            'email',
             'title',
             'content',
             'price',
